48712516a96d18ff8ad39f2521cf52794f748e5d
George Tohme <612096+djeetee@users.noreply.github.com>
2023-02-13 19:26:22 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <task
+      v-for="(task, index) in tasks"
+      :message="task.message"
+      :key="index"
+      :index="index"
+      :ref="'task' + index"
+    />
+  </div>
+</template>
+
+<script>
+import Task from "./components/Task.vue";
+// import LeaderLine from "leader-line-vue";
+
+export default {
+  name: "App",
+  components: { Task },
+  data() {
+    return {
+      tasks: [
+        { message: "hello" },
+        { message: "hello" },
+        { message: "hello" },
+        { message: "hello" },
+        { message: "hello" },
+        { message: "hello" },
+      ],
+      line1: undefined,
+    };
+  },
+  mounted() {
+    new LeaderLine(
+      document.querySelector("#task-0"),
+      document.querySelector("#task-1")
+    );
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: blue;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+}
+</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,39 @@
+<template>
+<div :id="`task-${index}`" class="tasks">
+  <p>
+    {{message}}
+  </p>
+</div>
+</template>
+
+<script>
+import Draggable from 'plain-draggable';
+export default {
+  name: "Task",
+  props: {
+    message: {
+      type: String
+    },
+    index: {
+      type: Number
+    }
+  },
+  data(){
+    return {
+      draggable:undefined,
+    }
+  },
+  mounted() {
+    this.draggable = new Draggable(document.querySelector(`#task-${this.index}`))
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.tasks {
+  background-color: #f0f0f0;
+  width: fit-content;
+  align-self: flex-start;
+}
+</style>
